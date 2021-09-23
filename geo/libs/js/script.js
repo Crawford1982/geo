5b7e1bcecc4fc4b5b5bae2bb07cd60faae51754b
James <59824155+Crawford1982@users.noreply.github.com>
2021-09-23 16:33:57 +0100

    link

--- geo/libs/js/script.js
+++ geo/libs/js/script.js
@@ -1,28 +1,29 @@
-	$('#btnRun').click(function() {
+$('#btnRun').click(function() {
 
-		$.ajax({
-			url: "geo/libs/php/getCountryInfo.php",
-			type: 'POST',
-			dataType: 'json',
-			data: {
-				title: $('#selTitle').val()
-				
-			},
-			success: function(result) {
-
-				console.log(JSON.stringify(result));
-
-				if (result.status.name == "ok") {
-
-					$('#txtSummary').html(result['data'][0]['summary']);
-					
-
-				}
+	$.ajax({
+		url: "geo/libs/php/findnearbyWiki.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			title: $('#selTitle').val()
 			
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				// your error code
+			
+		},
+		success: function(result) {
+
+			console.log(JSON.stringify(result));
+
+			if (result.status.name == "ok") {
+
+				$('#txtSummary').html(result['data'][0]['summary']);
+				
+
 			}
-		}); 
-	
-	});
+		
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			// your error code
+		}
+	}); 
+
+});
